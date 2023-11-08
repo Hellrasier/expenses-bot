b231ecb1a6a3e76593d33301ac0a2c08e9dd18fa
denislolipop48@gmail.com <deniska>
2023-11-08 17:07:00 -0500

    migrations - what I needed

---
+++ migrations/20231108220438_increase-id.sql
@@ -0,0 +1,25 @@
+-- Add migration script here
+
+PRAGMA foreign_keys=off;
+
+BEGIN TRANSACTION;
+
+CREATE TABLE IF NOT EXISTS expenses (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  username TEXT NOT NULL,
+  price REAL NOT NULL,
+  comments TEXT,
+  date TEXT NOT NULL
+);
+
+INSERT INTO new_expenses (user_id, username, price, comments, date)
+SELECT user_id, username, price, comments, date FROM expenses;
+
+DROP TABLE expenses;
+
+ALTER TABLE new_expenses RENAME TO expenses;
+
+COMMIT;
+
+PRAGMA foreign_keys=on;
