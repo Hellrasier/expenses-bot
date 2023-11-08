9bc5400e4341cc0279004d8eb4a071a4634c77f5
denislolipop48@gmail.com <deniska>
2023-11-08 17:21:01 -0500

    kek

--- migrations/20231108051933_init.sql
+++ migrations/20231108051933_init.sql
@@ -1,7 +1,7 @@
 -- Add migration script here
 
-CREATE TABLE IF NOT EXISTS expenses (
-  id INTEGER PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS new_expenses (
+  id SERIAL PRIMARY KEY,
   user_id INTEGER NOT NULL,
   username TEXT NOT NULL,
   price REAL NOT NULL,
